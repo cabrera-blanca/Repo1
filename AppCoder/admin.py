f47da172b73cb490920bbf01c2b414109b426932
Lucia Cabrera <cabrera.blanca@hotmail.com>
2024-03-31 12:36:37 -0300

    modulos nuevos ok

--- AppCoder/admin.py
+++ AppCoder/admin.py
@@ -4,3 +4,5 @@ from .models import *
 # Register your models here.
 
 admin.site.register(Curso)
+admin.site.register(Profesores)
+admin.site.register(Alumno)
--- AppCoder/models.py
+++ AppCoder/models.py
@@ -10,7 +10,7 @@ class Curso (models.Model):
 
 
     def __str__(self):
-        return  f"Nombre : {self.nombre}, Camada : {self.camada}"
+        return  f"Curso : {self.nombre}, Camada : {self.camada}"
     
 
 #-----Alumnos-------#
@@ -34,15 +34,16 @@ class Alumno (models.Model):
             return "Sin fecha de nacimiento"
         
     def __str__(self):
-        return f'{self.apellido} {self.nombre}'
+        return f'Alumno : {self.apellido} {self.nombre}'
 
 
 #-----Profesores------#
-class Profesor(models.Model):
-    alumno = models.OneToOneField(Alumno, parent_link=True, on_delete=models.CASCADE)
-    titulo = models.CharField(max_length=100)
+class Profesores(models.Model):
+    apellido = models.CharField(max_length=256, default="")
+    nombre = models.CharField(max_length=256)
+    curso = models.ForeignKey(Curso, on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self):
-        return f"{super().__str__()} - {self.titulo}"
+        return  f'Profesor: {self.apellido} {self.nombre}, {self.curso}'
             
     
